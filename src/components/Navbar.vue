f939031c5e33a264bf6e607710996244fd777faf
Aztim <barboza-82@mail.ru>
2022-01-10 00:08:31 +0300

    header-top in Navbar-component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+<header class="header">
+  <div class="container">
+    <div class="header__top">
+      <nav class="menu">
+        <a class="menu__link" href="#">Our Shops</a>
+        <a class="menu__link" href="#">Sale</a>
+        <a class="menu__link" href="#">About Us</a>
+      </nav>
+
+      <a class="logo" href="#">
+        <img class="logo__img" src="@/assets/img/icons/logo.svg" alt="logo" />
+        <img class="logo__img-mobile" src="@/assets/img/icons/logo-mobile.svg" alt="logo mobile" />
+      </a>
+      <a class="header-info" href="#">
+        <img class="header-info__img" src="@/assets/img/icons/geopoint.svg" alt="geopoint" />
+        <span class="header-info__text">Moscow, Arbat st. 55</span>
+      </a>
+
+      <ul class="user-list">
+        <li class="user-list__item" v-for="u in userList" :key="u.alt">
+          <a class="user-list__link" href="#">
+            <img class="user-list__img" :src="require(`@/assets/img/icons/${u.src}`)" :alt="u.alt" />
+          </a>
+        </li>
+
+        <!-- <li class="user-list__item">
+          <a class="user-list__link" href="#">
+            <img class="user-list__img" src="@/assets/img/icons/heart.svg" alt="favorites" />
+          </a>
+        </li>
+        <li class="user-list__item">
+          <a class="user-list__link" href="#">
+            <img class="user-list__img" src="@/assets/img/icons/user.svg" alt="user" />
+          </a>
+        </li>
+        <li class="user-list__item">
+          <a class="user-list__link" href="#">
+            <img class="user-list__img" src="@/assets/img/icons/basket.svg" alt="basket" />
+            <span class="user-list__count">1</span>
+          </a>
+        </li> -->
+      </ul>
+    </div>
+  </div>
+</header>
+</template>
+
+<script>
+import { } from 'vue'
+
+export default {
+  setup () {
+    const userList = ([
+      { class: 'user-list__img', src: 'heart.svg', alt: 'favorites' },
+      { class: 'user-list__img', src: 'user.svg', alt: 'user' },
+      { class: 'user-list__img', src: 'basket.svg', alt: 'basket' }
+    ])
+
+    return { userList }
+  }
+}
+</script>
+
+<style>
+
+</style>
