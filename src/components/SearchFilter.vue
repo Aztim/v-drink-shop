a4df8e6486e0b1eb025b20e010c0baa4c63fe19a
Aztim <barboza-82@mail.ru>
2022-01-12 01:16:50 +0300

    started SearchFilter-component

---
+++ src/components/SearchFilter.vue
@@ -0,0 +1,45 @@
+<template>
+<section class="search">
+  <div class="container">
+    <div class="search-tabs">
+      <div class="search-tabs__header">
+        <div class="mobile-overflow">
+          <div class="search-tabs__title active">Поиск по номеру</div>
+          <div class="search-tabs__title">Поиск по марке</div>
+          <div class="search-tabs__title">Поиск по названию товара</div>
+        </div>
+      </div>
+
+      <div class="search-tabs__content-header">
+        <div
+        v-for="s in searchForm"
+        :key="s.placeholder"  class="search-tabs__content-item" :class="s.class" >
+          <form class="search-tabs__content-form" action="get">
+            <input class="search-tabs__content-input" type="text" placeholder="Введите номер">
+            <button class="search-tabs__content-btn" type="submit">Искать</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  setup () {
+    const searchForm = ([
+      { placeholder: 'Введите номер', class: 'active' },
+      { placeholder: 'Введите марку' },
+      { placeholder: 'Введите название' }
+
+    ])
+
+    return { searchForm }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
     <PromoCarousel />
+    <SearchFilter />
+
   </div>
 </template>
 
 <script lang="ts">
 import PromoCarousel from '@/components/ui/PromoCarousel.vue'
+import SearchFilter from '@/components/SearchFilter.vue'
 
 export default {
   name: 'Home',
   components: {
-    PromoCarousel
+    PromoCarousel,
+    SearchFilter
   }
 }
 </script>
