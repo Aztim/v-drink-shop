55ce2c5e10f1f0832c94404f7ebd86f712228529
Aztim <barboza-82@mail.ru>
2022-01-12 15:14:43 +0300

    started Categories-component

---
+++ src/components/Categories.vue
@@ -0,0 +1,40 @@
+<template>
+<section class="categories">
+  <div class="container">
+    <div class="categories-wrapper">
+
+      <a class="categories-item" href="#" v-for="c in categoriesTitle" :key="c.title">
+        <div class="categories-item__text">
+          <h4 class="categories-item__title">{{ c.title }}</h4>
+          <p class="categories-item__subtitle">Подробнее</p>
+        </div>
+        <div class="categories-item__img">
+          <img :src="require(`@/assets/img/${c.img}`)" alt="categories">
+        </div>
+      </a>
+
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  setup () {
+    const categoriesTitle = ([
+      { title: 'Vodka', img: 'categories-1.png' },
+      { title: 'Liquor', img: 'categories-1.png' },
+      { title: 'Beer', img: 'categories-1.png' },
+      { title: 'Cider', img: 'categories-1.png' },
+      { title: 'Gin', img: 'categories-1.png' },
+      { title: 'Rum', img: 'categories-1.png' }
+    ])
+
+    return { categoriesTitle }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,21 @@
   <div>
     <PromoCarousel />
     <SearchFilter />
-
+    <Categories />
   </div>
 </template>
 
 <script lang="ts">
 import PromoCarousel from '@/components/ui/PromoCarousel.vue'
 import SearchFilter from '@/components/SearchFilter.vue'
+import Categories from '@/components/Categories.vue'
 
 export default {
   name: 'Home',
   components: {
     PromoCarousel,
-    SearchFilter
+    SearchFilter,
+    Categories
   }
 }
 </script>
